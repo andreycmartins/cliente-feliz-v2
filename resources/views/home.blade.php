50ea8db005613d5a267438323f21ebb4e2ef33f3
Andrey Martins <andrey.costs@versatecnologia.com.br>
2023-11-29 09:14:01 -0300

    DataTable funcionando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,12 +83,14 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="//cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 <script>
-    let table = new DataTable('#list-users', {
-        // "search": {
-        //     "search": "Andrey"
-        // }
-    });
     $(document).ready(function() {
+
+        let table = new DataTable('#list-users', {
+            // "search": {
+            //     "search": "Andrey"
+            // }
+        });
+        
         $('#create-user-btn').click(function() {
             $('#create-user-modal').modal('show');
         });
